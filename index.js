e32724f5257feee9074ab746db1815caf376af29
Shittu Nuha <156665730+King-Nushad@users.noreply.github.com>
2024-05-01 14:19:06 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById('check').addEventListener('change', function() {
+    document.querySelector('.checkbtn').classList.toggle('fa-bars');
+    document.querySelector('.checkbtn').classList.toggle('fa-times');
+});
+
